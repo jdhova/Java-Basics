0ec4991e5d98d705712ff2b1c6eac4c249939d75
jdhova <juud0010@gmail.com>
2024-08-12 20:12:10 -0300

    class

---
+++ Main.java
@@ -0,0 +1,188 @@
+// import java.math.BigInteger;
+
+// class Hello 
+// {
+//     public static void main(String a[])
+//     {
+//        int num1 = 20;
+//        float marks = 4.3f;
+//         char chh = 'k';
+//         double mark = 623333333;
+//         byte by = 127;
+//         short sh = 588;
+//         long l = 822218222;
+//         boolean b = true;
+//         BigInteger reallyBig  =  new BigInteger("1234567890123456890"+ "30");
+//         String str =  "Yess";
+//         String strs =  "Noo";
+        
+        
+
+//         num1 = 10+ 20;
+//         num1 = 22;
+//         chh = 'f';
+//         mark = 7.222222;
+//         by = 126;
+//         sh = 12345;
+//         l = 1234567890;
+//         b = false;
+//         str = "Hello World this is a code";
+//         String.join(str+strs);
+//         String.join(str,strs,"yellow");
+    
+
+    
+
+//         // System.out.println(num1 + marks);
+//         // System.out.println(num1);
+//         // System.out.println(marks);
+//         // System.out.println(chh);
+//         // System.out.println(mark);
+//         // System.out.println(by);
+//         // System.out.println(sh);
+//         // System.out.println(l);
+//         // System.out.println(b);
+//         // System.out.println(reallyBig);
+//         // System.out.println("YES");
+//         // System.out.println( String.join(str+strs));
+
+//     }
+    
+// }
+
+// // Adding
+
+
+// class Hello2 
+// {
+//     public static void main(String a[])
+//     {   
+//         for (int i = 1; i <= 3; i++) {
+//             System.out.println("Mercedes: " + i); // Executes 2 times
+            
+//             // Inner loop
+//             for (int j = 1; j <= 3; j++) {
+//               System.out.println(" C-Class: " + j); // Executes 6 times (2 * 3)
+//             }
+//           } 
+        
+
+
+//     }
+    
+// }
+
+
+
+
+//  class Hellos
+// {
+//     public static void main(String a[])
+//     {   
+//         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+//         for (String i : cars) {
+//           System.out.println(i);
+//         }
+        
+
+//     }
+    
+// }
+
+// public class Hellss {
+//     public static void main(String[] args) {
+//       int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+//       for (int i = 0; i < myNumbers.length; ++i) {
+//          for(int j = 0; j < myNumbers[i].length; ++j) {
+//             System.out.println(myNumbers[i][j]);
+//          }
+//       }
+//     }
+
+
+public class Main {
+  int x = 500;
+  int y = 50;
+  char l ='f';
+ String name = "Jude";
+ String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+  public static void main(String[] args) {
+
+    Main myObj1 = new Main();  // Object 1
+    Main myObj2 = new Main();  // Object 2
+    System.out.println(myObj1.x);
+    System.out.println(myObj1.y);
+    System.out.println(myObj1.l);
+    System.out.println(myObj1.name);
+    // System.out.println(myObj2.cars);
+  }
+}
+
+class Second {
+  public static void main(String[] args) {
+    Main myObj1 = new Main();
+    System.out.println(myObj1.x);
+  }
+}
+
+class Third {
+  public static void main(String[] args) {
+    Main myObj1 = new Main();
+    System.out.println(myObj1.name);
+  }
+}
+
+class fourth {
+  public static void main(String[] args) {
+    Main myObj1 = new Main();
+    System.out.println(myObj1.l);
+  }
+}
+
+class Fifth {
+  public static void main(String[] args) {
+    Main myObj1 = new Main();
+    System.out.println(myObj1.cars);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DIFFRENCE BETWEEN DOUBLE AND INT AND FLOAT
+//HOW DO I WRITE MORE THAN 1O DIGITS IN JAVA
+// byte is the smaller version of int
+// what values data types are mutble and what values are immutable?
+// We join strings with , and not + in Java
+//max Byte is 256 anyhting else it devides and send the reminder
+//casting and promotion
